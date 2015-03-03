999ae9770d08ab9841a60287c9987100bf89432c
Simao Belchior <simaobelchior@gmail.com>
2015-03-03 16:31:48 +0000

    Added index page where endpoints will be explained

---
+++ src/main/java/org/unepwcmc/taxonifyapi/resources/TaxaResource.java
@@ -0,0 +1,30 @@
+package org.unepwcmc.taxonifyapi.resources;
+
+import com.google.common.base.Optional;
+import org.unepwcmc.taxonifyapi.dao.Taxon;
+import org.unepwcmc.taxonifyapi.dao.TaxonDAO;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * Created by Simao on 03/03/15.
+ */
+@Path("/taxa")
+@Produces(MediaType.APPLICATION_JSON)
+public class TaxaResource {
+    private final TaxonDAO dao;
+
+    public TaxaResource(TaxonDAO dao) {
+        this.dao = dao;
+    }
+
+    @GET
+    public List<Taxon> scientificName(@QueryParam("scientificName") Optional<String> scientificName) {
+        return scientificName.isPresent() ? dao.findByScientificName(scientificName.get() + "%") : dao.findAll();
+    }
+}
