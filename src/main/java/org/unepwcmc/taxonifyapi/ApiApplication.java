1790563eb812123784e42c33a27337ed815092a1
Simao Belchior <simaobelchior@gmail.com>
2015-03-03 15:00:47 +0000

    Adding an index.html page

--- src/main/java/org/unepwcmc/taxonifyapi/ApiApplication.java
+++ src/main/java/org/unepwcmc/taxonifyapi/ApiApplication.java
@@ -1,6 +1,7 @@
 package org.unepwcmc.taxonifyapi;
 
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -12,9 +13,14 @@ import org.unepwcmc.taxonifyapi.resources.TaxonResource;
  * Created by Simao on 02/03/15.
  */
 public class ApiApplication extends Application<ApiConfiguration> {
+    @Override
+    public String getName() {
+        return "taxonify-api";
+    }
 
     @Override
     public void initialize(Bootstrap<ApiConfiguration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/app", "/", "index.html"));
     }
 
     public static void main(String[] args) throws Exception {
