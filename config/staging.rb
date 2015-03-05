4e0d336dc894017058374d0ee1263177ae50f6fc
strtwtsn <stuart.watson@unep-wcmc.org>
2015-03-05 12:43:13 +0000

    Delete staging.rb

--- config/staging.rb
+++
@@ -1,5 +0,0 @@
-set :rails_env, "staging"
-# Primary domain name of your application. Used in the Apache configs
-role :web, "ec2-54-94-97-96.sa-east-1.compute.amazonaws.com" # Your HTTP server, Apache/etc
-role :app, "ec2-54-94-97-96.sa-east-1.compute.amazonaws.com" # This may be the same as your `Web` server
-role :db, "ec2-54-94-97-96.sa-east-1.compute.amazonaws.com", :primary => true # This is where Rails migrations wil
