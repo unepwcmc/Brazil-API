0d1f148c2d98121ee49119335b9554179027d927
Simao Belchior <simaobelchior@gmail.com>
2015-03-05 11:37:59 +0000

    Added Cross Origin Domain requests support

--- src/main/java/org/unepwcmc/taxonifyapi/ApiApplication.java
+++ src/main/java/org/unepwcmc/taxonifyapi/ApiApplication.java
@@ -5,6 +5,7 @@ import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.skife.jdbi.v2.DBI;
 import org.unepwcmc.taxonifyapi.dao.autocomplete.AutocompleteDAO;
 import org.unepwcmc.taxonifyapi.dao.common_name.CommonNamesDAO;
@@ -12,6 +13,10 @@ import org.unepwcmc.taxonifyapi.dao.distribution.DistributionDAO;
 import org.unepwcmc.taxonifyapi.dao.taxa.TaxaDAO;
 import org.unepwcmc.taxonifyapi.dao.taxon.TaxonDAO;
 import org.unepwcmc.taxonifyapi.resources.*;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 /**
  * Created by Simao on 02/03/15.
@@ -34,6 +39,15 @@ public class ApiApplication extends Application<ApiConfiguration> {
     @Override
     public void run(ApiConfiguration config, Environment environment) throws Exception {
         environment.jersey().setUrlPattern("/api/*");
+
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
         final DBIFactory factory = new DBIFactory();
         final DBI jdbi = factory.build(environment, config.getDataSourceFactory(), "postgresql");
